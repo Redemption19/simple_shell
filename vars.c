10c37134fa0c83e928649bee2d1add127e6cd808
Katherine <106969330+Ewuraa@users.noreply.github.com>
2022-09-07 12:40:47 +0000

    Update vars.c

--- vars.c
+++ vars.c
@@ -18,7 +18,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		j++;
 		info->cmd_buf_type = CMD_OR;
 	}
-	els if (buf[j] == '&' && buf[j + 1] == '&')
+	else if (buf[j] == '&' && buf[j + 1] == '&')
 	{
 		buf[j] = 0;
 		j++;
